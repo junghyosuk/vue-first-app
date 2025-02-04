a1143b31585c1eea78f0748c869dbfdf6d8db4ae
hyosukjung <hyosukjung32@gmail.com>
2025-02-05 01:03:38 +0900

    vue 프로젝트 생ㅓ

---
+++ vue_project/src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <div id="wrapHead" class="wrap_head">
+      <!-- 로그인 상태에 따라 다른 UI 표시 -->
+      <div v-if="!isLoggedIn">
+        <a href="#" class="link_top">로그인</a>
+        <a href="#" class="link_top">회원가입</a>
+      </div>
+      <div v-else>
+        <span>{{ userName }}님, 환영합니다.</span>
+        <a href="#" class="link_top">로그아웃</a>
+      </div>
+      <!-- 기타 헤더 내용 -->
+    </div>
+  </header>
+</template>
